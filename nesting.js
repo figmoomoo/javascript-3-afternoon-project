390aeea38545ee24378bef54cff3c516cb413f23
figmoomoo <figmoomoo@gmail.com>
2020-07-13 02:49:50 -0500

    partially done

--- nesting.js
+++ nesting.js
@@ -52,8 +52,17 @@ var employees = [
 
 //Code Here
 function employeeUpdater(){
-  if()
+  for(let i = 0; i < employees.length; i++){
+    if(employees[i].firstName === "Theo"){
+      employees.splice(i,1);
+    }
+    else if(employees[i].firstName = "Lorie"){
+      employees[i].department = "HR"
+    }
+  }
+  return employees;
 }
+
 
 
 ////////// PROBLEM 2 //////////
@@ -71,7 +80,9 @@ var workplaceAccidents = [12, 56, 44, 3, 29, 56, 56, 3, 7, 12];
 */
 
 //Code Here
-
+function removeDuplicates(){
+  
+}
 
 
 ////////// PROBLEM 3 //////////
